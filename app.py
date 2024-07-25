3a04e5227df469e5086b6b8a9f144b8afa9da949
Mitesh Gupta <mgmiteshgupta134@gmail.com>
2024-07-25 08:20:23 +0530

    App

--- app.py
+++ app.py
@@ -1,7 +1,46 @@
 import streamlit as st
+import pdfplumber
 
 st.title("Resume Parsing App")
 
-resume=st.file_uploader("Upload CV/Resume",['pdf','docx','txt'])
+uploaded_resume=st.file_uploader("Upload CV/Resume",['pdf','docx','txt'])
 
-print(resume)
+def check_document_type(doc):
+    pass
+
+def extract_text_from_pdf(pdf_file):
+    text = ""
+    with pdfplumber.open(pdf_file) as pdf:
+        print("Hello")
+        for page in pdf.pages:
+            text += page.extract_text()
+        formated_text=text.replace('\n',' ')
+    return formated_text
+
+def extract_text_from_txt(text_file):
+    pass
+
+def extract_text_from_docx(word_file):
+    pass
+
+def preprocess_text():
+    pass
+
+def extract_entities():
+    pass
+
+if uploaded_resume is not None:
+    doc_type=check_document_type(uploaded_resume)
+    if doc_type=='invalid':
+        st.error("CV/Resume should be in PDF/DOCX/TXT format")
+    else:
+        if doc_type=='application/pdf':
+            text=extract_text_from_pdf(uploaded_resume)
+        elif doc_type=='text/plain':
+            text=extract_text_from_docx(uploaded_resume)
+        else:
+            text=extract_text_from_docx(uploaded_resume)
+    if text:
+        preprocessed_text=preprocess_text(text)
+        entities=extract_entities(preprocessed_text)
+        
