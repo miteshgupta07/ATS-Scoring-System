e1d8f2de2701d414ce0674a0942f98a780737a17
Mitesh Gupta <mgmiteshgupta134@gmail.com>
2024-07-29 21:09:43 +0530

    Final Code Changes Done

--- app.py
+++ app.py
@@ -158,8 +158,28 @@ def show_entities(text):
     df = pd.DataFrame(data, columns=["Label", "Entity"])
     st.table(df)
 
-def show_suggestion():
-    pass
+def show_suggestion(keywords):
+    improvement_suggestions = {
+        "NAME": "• **Name:** Including your full name helps recruiters easily identify you.",
+        "EMAIL_ADDRESS": "• **Email Address:** Providing an email address makes it easier for recruiters to contact you.",
+        "LOCATION": "• **Location:** Including your location helps recruiters find candidates in specific geographic areas.",
+        "DEGREE": "• **Degree:** Listing your degree demonstrates your educational qualifications.",
+        "GRADUATION_YEAR": "• **Graduation Year:** Including your graduation year helps recruiters understand your experience level.",
+        "COLLEGE_NAME": "• **College/University Name:** Mentioning your college or university name adds credibility and context to your educational background.",
+        "SKILLS": "• **Skills:** Highlighting your skills shows your expertise and can make your resume stand out.",
+        "WORK_EXPERIENCE": "• **Work Experience:** Detailing your previous job roles and responsibilities provides insight into your professional background.",
+        "CERTIFICATIONS": "• **Certifications:** Adding relevant certifications can showcase your additional qualifications and specialized knowledge.",
+        "PROJECTS": "• **Projects:** Describing significant projects you have worked on can demonstrate your practical experience and problem-solving abilities.",
+        "LANGUAGES": "• **Languages:** Including languages you are proficient in can be advantageous, especially for roles requiring multilingual skills.",
+        "LINKEDIN_PROFILE": "• **LinkedIn Profile:** Providing a link to your LinkedIn profile can give recruiters a more comprehensive view of your professional network and endorsements.",
+        "PROFESSIONAL_SUMMARY": "• **Professional Summary:** Writing a concise professional summary at the top of your resume can quickly convey your key strengths and career objectives.",
+        "DESIGNATION": "• **Designation:** Specifying your current or desired job title helps recruiters understand your career level and aspirations."
+        }
+    
+    st.write("#### You can add the following details to improve your score:")
+    for i in keywords:
+        st.write(improvement_suggestions[i])
+
 def main(uploaded_resume,job_description):
     if uploaded_resume is not None:
         doc_type=uploaded_resume.type
@@ -189,22 +209,26 @@ def main(uploaded_resume,job_description):
             time.sleep(1)
 
             ats_score,not_found_keywords=show_ATS_score(text,job_description)
+
             if ats_score==-1:
                 st.warning("No Keywords Found in Job Description")
             else:
-                if ats_score<=50:
+                if 0<ats_score<=50:
                     st.error(f"Your ATS Score is{ats_score} out of 100")
-                elif ats_score<=75:
+                    st.write("Your score is Low")
+
+                elif 50<ats_score<=75:
                     st.warning(f"Your ATS Score is {ats_score} out of 100")
+                    st.write("Your score is Average")
+
                 else:
                     st.success(f"Your ATS Score is {ats_score} out of 100")
+                    st.write("Your score is Good")
+
             
-            st.write('## *Suggestion Based on your Resume*')
-            if len(not_found_keywords)==0:
-                st.write("• Your Resume looks good and it has all the necessary keywords for this job description")
-            else:
-                s=", ".join(not_found_keywords)
-                st.write('You should add some more keywords like',s.title())
+            if len(not_found_keywords)!=0:
+                st.write('## *Suggestion Based on your Resume*')
+                show_suggestion(not_found_keywords)
 
             if st.checkbox('Show All Entities'):
                 col1,col2=st.columns(2)
